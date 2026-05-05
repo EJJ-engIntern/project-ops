6527f89d50233463e08f77010f62c594f8f9d1c0
Emmanuel James <Emmanuel.James@varite.com>
2026-05-05 16:58:48 +0530

    more improv for hosting

--- frontend/src/api/axios.ts
+++ frontend/src/api/axios.ts
@@ -1,26 +1,13 @@
-// import axios from 'axios';
-
-// const api = axios.create({ baseURL: import.meta.env.VITE_API_URL });
-
-// api.interceptors.request.use(config => {
-//   const token = localStorage.getItem('token');
-//   if (token) config.headers.Authorization = `Bearer ${token}`;
-//   return config;
-// });
-
-// export default api;
-
-
 import axios from 'axios';
 
-//const api = axios.create({ baseURL: import.meta.env.VITE_API_URL });
-const api = axios.create({ baseURL: 'http://localhost:5000/api' });
-console.log('API URL:', import.meta.env.VITE_API_URL);
+const api = axios.create({
+baseURL: 'https://project-ops.onrender.com/api'
+});
 
 api.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
+const token = localStorage.getItem('token');
+if (token) config.headers.Authorization = `Bearer ${token}`;
+return config;
 });
 
 export default api;
